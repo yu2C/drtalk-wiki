34d330958814ed188d0eab8fcabed55060249763
yu2C <c2001413@gmail.com>
2026-06-22 21:01:50 +0800

    config: use workers dev base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "drtalk-wiki.c2001413.workers.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
